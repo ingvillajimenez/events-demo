a2da9b69c91ba10f0a97fe5efeedbda4692a58f9
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-18 16:25:47 -0600

    The Joys of the Method Binding :(

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import WiseSquare from "./WiseSquare";
 import "./App.css";
 import AnnoyingForm from "./AnnoyingForm";
 import CopyDemo from "./CopyDemo";
+import WiseSquareWithProps from "./WiseSquareWithProps";
 
 function App() {
   return (
     <div className="App">
       <h1>React Events!!</h1>
       <WiseSquare />
-      <AnnoyingForm />
-      <CopyDemo />
+      <WiseSquareWithProps />
     </div>
   );
 }
